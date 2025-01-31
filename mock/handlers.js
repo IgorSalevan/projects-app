276f36f3d9625480f153070dede9d834da2c5bb9
igor.suleimanov <igor.salevan@gmail.comm>
2025-01-31 17:33:04 +0100

    Add missing mock api requests for favourite projects

--- mock/handlers.js
+++ mock/handlers.js
@@ -4,30 +4,42 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const data = await readFile(new URL('./data/projects.json', import.meta.url), 'utf-8');
-const projectsJson = JSON.parse(data);
+const data = await readFile(new URL('./data.json', import.meta.url), 'utf-8');
+const dataJson = JSON.parse(data);
 
 const apiProjectsPath = `${process.env.NEXT_PUBLIC_API_MOCK_URL}/projects`;
+const apiFavouritesPath = `${process.env.NEXT_PUBLIC_API_MOCK_URL}/favourites`;
 
 const projects = new Map([
-  ...projectsJson.projects.map((project) => [project.id, project]),
+  ...dataJson.projects.map((project) => [project.id, project]),
+]);
+
+const favourites = new Map([
+  ...dataJson.favourites.map((item) => [
+    item.projectId,
+    projects.get(item.projectId),
+  ]),
 ]);
 
 export const handlers = [
-  http.get(apiProjectsPath, async () => {
+  http.get(apiProjectsPath, () => {
     return HttpResponse.json(Array.from(projects.values()));
   }),
 
-  http.get(`${apiProjectsPath}/:id`, async ({ _, params }) => {
+  http.get(`${apiProjectsPath}/:id`, ({ _, params }) => {
     const { id } = params;
 
     const project = projects.get(id);
 
     if (!project) {
-      return new HttpResponse(null, {
-        status: 404,
-        statusText: `Project does not exist, id: ${projectId}`,
-      });
+      return HttpResponse.json(
+        {
+          error: 'Project not found',
+        },
+        {
+          status: 404,
+        }
+      );
     }
     return HttpResponse.json(project);
   }),
@@ -35,6 +47,43 @@ export const handlers = [
   http.post(apiProjectsPath, async ({ request }) => {
     const project = await request.json();
     projects.set(project.id, project);
-    return HttpResponse.json(project);
+    return HttpResponse.json(project, {status: 201});
+  }),
+
+  http.put(`${apiProjectsPath}/:id`, async ({ request, params }) => {
+    const {id} = params;
+    const project = await request.json();
+    projects.set(id, project);
+    return HttpResponse.json(project, {status: 204});
+  }),
+
+  // Favourites
+  http.get(apiFavouritesPath, () => {
+    return HttpResponse.json(Array.from(favourites.values()));
+  }),
+
+  http.post(apiFavouritesPath, async ({ request }) => {
+    const { projectId } = await request.json();
+
+    if (!projectId) {
+      return res(
+        ctx.status(400),
+        ctx.json({ error: 'Project ID is required' })
+      );
+    }
+
+    const project = projects.get(projectId);
+    favourites.set(projectId, project);
+
+    return HttpResponse.json(project, {status: 201});
+  }),
+
+  http.delete(`${apiFavouritesPath}:projectId`, ({ params }) => {
+    const { projectId } = params;
+
+    if (favourites.delete(projectId)) {
+    }
+
+    return HttpResponse.json({message: 'Project removed from favourites'}, {status: 204});
   }),
 ];
